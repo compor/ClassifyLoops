53def43c727badb6cc247930b44b94d47874b27d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-04-14 16:41:08 +0100

    simplify and improve control flow

--- cmake/modules/Python.cmake
+++ cmake/modules/Python.cmake
@@ -1,23 +1,35 @@
+# cmake file
+
 include(FindPackageHandleStandardArgs)
 include(FindPythonInterp)
 
-function(find_python_module module)
-  string(TOUPPER ${module} module_upper)
-  if(NOT PY_${module_upper})
-    # A module's location is usually a directory, but for binary modules
-    # it's a .so file.
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
-      "import re, ${module}; print re.compile('/__init__.py.*').sub('',${module}.__file__)"
-      RESULT_VARIABLE _${module}_status
-      OUTPUT_VARIABLE _${module}_location
-      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if(NOT _${module}_status)
-      set(PY_${module_upper} ${_${module}_location} CACHE STRING
-        "Location of Python module ${module}")
-    endif(NOT _${module}_status)
-  endif(NOT PY_${module_upper})
-  find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
-  if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED" AND NOT PY_${module_upper})
-    message(FATAL_ERROR "Python module ${module} not found")
+function(find_python_module name)
+  set(pkg_name "PY_${name}")
+  string(TOUPPER "PY_${name}" module)
+
+  set(status FALSE)
+
+  # a module's location is usually a directory,
+  # but for binary modules it's a .so file
+  set(prg_str "import re, ${name}; \
+  print re.compile('/__init__.py.*').sub('',${name}.__file__)")
+
+  if(PYTHONINTERP_FOUND)
+    execute_process(COMMAND
+      "${PYTHON_EXECUTABLE} -c ${prg_str}"
+      RESULT_VARIABLE status
+      OUTPUT_VARIABLE location
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+  else()
+    message(WARNING "Could not find Python interpreter")
   endif()
-endfunction(find_python_module)
+
+  if(NOT status)
+    set(${module} ${location}
+      CACHE STRING "Location of Python module: ${name}")
+  endif()
+
+  find_package_handle_standard_args(${pkg_name} DEFAULT_MSG ${module})
+endfunction()
+
