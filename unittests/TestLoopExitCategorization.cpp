08024f2e7d0da898ef13d5c0a2c92e0118bc3ae7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-04-18 21:57:44 +0100

    add loop pass handling

--- unittests/TestLoopExitCategorization.cpp
+++ unittests/TestLoopExitCategorization.cpp
@@ -8,6 +8,18 @@
 #include "llvm/IR/Module.h"
 // using llvm::Module
 
+#include "llvm/IR/LegacyPassManager.h"
+// using llvm::legacy::PassMananger
+
+#include "llvm/PassRegistry.h"
+// using llvm::PassRegistry
+
+#include "llvm/Pass.h"
+// using llvm::Pass
+
+#include "llvm/Analysis/LoopInfo.h"
+// using llvm::LoopInfoWrapperPass
+
 #include "llvm/Support/SourceMgr.h"
 // using llvm::SMDiagnostic
 
@@ -25,26 +37,37 @@
 namespace {
 
 class LoopExitClassifierTest : public testing::Test {
+public:
+  LoopExitClassifierTest() : m_FPM(nullptr) {
+    llvm::initializeLoopInfoWrapperPassPass(
+        *llvm::PassRegistry::getPassRegistry());
+
+    m_LP = std::make_unique<llvm::LoopInfoWrapperPass>();
+    m_FPM.add(m_LP.get());
+  }
+
 protected:
   void ParseAssembly(const char *Assembly) {
     llvm::SMDiagnostic Error;
-    M = llvm::parseAssemblyString(Assembly, Error, llvm::getGlobalContext());
+    m_Module = llvm::parseAssemblyString(Assembly, Error, llvm::getGlobalContext());
 
     std::string errMsg;
     llvm::raw_string_ostream os(errMsg);
     Error.print("", os);
 
-    if (!M)
+    if (!m_Module)
       llvm::report_fatal_error(os.str().c_str());
 
-    llvm::Function *F = M->getFunction("foo");
+    llvm::Function *F = m_Module->getFunction("foo");
     if (!F)
       llvm::report_fatal_error("Test must have a function named @foo");
 
     return;
   }
 
-  std::unique_ptr<llvm::Module> M;
+  std::unique_ptr<llvm::Module> m_Module;
+  std::unique_ptr<llvm::LoopInfoWrapperPass> m_LP;
+  llvm::legacy::FunctionPassManager m_FPM;
 };
 
 } // namespace unnamed end
