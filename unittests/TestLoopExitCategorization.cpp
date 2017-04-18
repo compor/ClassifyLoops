0b46e9d74b5180de378f223bc0aa3ddb315bac99
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-04-18 17:53:03 +0100

    add basic test fixture

--- unittests/TestLoopExitCategorization.cpp
+++ unittests/TestLoopExitCategorization.cpp
@@ -1,7 +1,53 @@
 
+#include <memory>
+// using std::unique_ptr
+
+#include "llvm/IR/LLVMContext.h"
+// using llvm::LLVMContext
+
+#include "llvm/IR/Module.h"
+// using llvm::Module
+
+#include "llvm/Support/SourceMgr.h"
+// using llvm::SMDiagnostic
+
+#include "llvm/AsmParser/Parser.h"
+// using llvm::parseAssemblyString
+
+#include "llvm/Support/ErrorHandling.h"
+// using llvm::report_fatal_error
+
+#include "llvm/Support/raw_ostream.h"
+// using llvm::raw_string_ostream
+
 #include "gtest/gtest.h"
 
-TEST(TestFoo, foo) { EXPECT_TRUE(true); }
+namespace {
+
+class LoopExitClassifierTest : public testing::Test {
+protected:
+  void ParseAssembly(const char *Assembly) {
+    llvm::SMDiagnostic Error;
+    M = llvm::parseAssemblyString(Assembly, Error, llvm::getGlobalContext());
+
+    std::string errMsg;
+    llvm::raw_string_ostream os(errMsg);
+    Error.print("", os);
+
+    if (!M)
+      llvm::report_fatal_error(os.str().c_str());
+
+    llvm::Function *F = M->getFunction("foo");
+    if (!F)
+      llvm::report_fatal_error("Test must have a function named @foo");
+
+    return;
+  }
+
+  std::unique_ptr<llvm::Module> M;
+};
+
+} // namespace unnamed end
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
