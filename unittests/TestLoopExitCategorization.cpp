fc991e3f908cce4fc4e21ae5b3f0df57afd2c096
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-04-26 17:29:51 +0100

    generate script to build and run unit tests

--- unittests/TestLoopExitCategorization.cpp
+++ unittests/TestLoopExitCategorization.cpp
@@ -195,10 +195,12 @@ TEST_F(TestClassifyLoopExits, InfiniteLoop) {
                   "%a = alloca i32, align 4\n"
                   "store i32 0, i32* %a, align 4\n"
                   "br label %1\n"
+
                   "%2 = load i32, i32* %a, align 4\n"
                   "%3 = add nsw i32 %2, 1\n"
                   "store i32 %3, i32* %a, align 4\n"
                   "br label %1\n"
+
                   "ret void\n"
                   "}\n");
 
